5479b0564867ee48ea364c53757db7cf0eeb5450
jhouston0411 <john.houston0411@gmail.com>
2022-06-15 17:59:17 -0500

    have finished logic for using locale storage now moving back to mainbody

--- script.js
+++ script.js
@@ -28,19 +28,32 @@ function divider(name) {
 // personal.addTodo(todo2);
 // dividers.push(personal);
 // dividers.push(gym);
+//
+// for(let i in dividers){
+//
+//   if(dividers[i].dividername === 'personal')
+//     dividers.splice(i, 1);
+// }
 
+window.addEventListener("load", () => {
+  let storageDividers = JSON.parse(localStorage.getItem("dividers"));
+  let tempName;
+  for (let divider in storageDividers) {
+    createDividers(storageDividers[divider].dividername);
+  }
+})
 // event to add a new divider
 document.addEventListener("click", (event) => {
   if (event.target.classList.contains("add-divider-btn")) {
     document.querySelector(".add-divider-btn").classList.add("hideAddDivider")
 
-    let newDivider = document.createElement("div");
-    newDivider.classList.add("divider-format")
-    newDivider.setAttribute("Id", "divider-format")
+    let newDividerSection = document.createElement("div");
+    newDividerSection.classList.add("divider-format")
+    newDividerSection.setAttribute("Id", "divider-format")
     let dividerInput = document.createElement("input");
     dividerInput.setAttribute("type", "text")
     dividerInput.classList.add("divider-input")
-    newDivider.append(dividerInput);
+    newDividerSection.append(dividerInput);
 
     let dividerSection = document.createElement("div");
     dividerSection.classList.add("divider-section-format")
@@ -53,31 +66,19 @@ document.addEventListener("click", (event) => {
     dividerSection.append(enter);
     dividerSection.append(exit);
 
-    newDivider.append(dividerSection)
-    document.querySelector(".add-divider-btn").before(newDivider)
+    newDividerSection.append(dividerSection)
+    document.querySelector(".add-divider-btn").before(newDividerSection)
     event.stopPropagation();
   }
 })
-//event to name the divider or cancel
+
+//event to confirm and name the divider or cancel new divider
 document.addEventListener("click", (event) => {
 
   if (event.target.classList.contains("enter-green-btn")) {
     const newDivider = document.querySelector(".divider-input").value;
     if (newDivider != "") {
-      let temp = new divider(newDivider);
-      dividers.push(temp);
-      let newLi = document.createElement("li");
-      newLi.classList.add("side-li")
-      let div = document.createElement("div");
-      div.setAttribute("class", "divider-name")
-      div.append(document.createTextNode(newDivider))
-      let i = document.createElement("i")
-      i.setAttribute("class", "delete-divider")
-      i.classList.add("fa-solid");
-      i.classList.add("fa-x")
-      newLi.append(div);
-      newLi.append(i)
-      document.querySelector(".divider-ul-list").append(newLi);
+      createDividers(newDivider);
       document.querySelector("#divider-format").remove()
       document.querySelector(".add-divider-btn").classList.remove("hideAddDivider")
     } else {
@@ -94,32 +95,73 @@ document.addEventListener("click", (event) => {
 document.addEventListener("click", (event) => {
   if (event.target.classList.contains("fa-x")) {
     let delete_divider_name = event.target.previousElementSibling;
+    let divider_name_content = delete_divider_name.textContent;
+
     let divider_parent_node = event.target.parentNode;
-    dividers.splice(dividers.indexOf(delete_divider_name), 1);
+    for (let i in dividers) {
+      if (dividers[i].dividername === divider_name_content)
+        dividers.splice(i, 1);
+    }
+    localStorage.setItem('dividers', JSON.stringify(dividers));
     divider_parent_node.remove();
+
+
+    //if the divider is opened in main-body delete that as well
+    let mainBody = document.querySelector(".main-body").firstElementChild;
+    let todo_divider = document.querySelector(".todo-divider-tab")
+
+    if (todo_divider != null && todo_divider.firstElementChild.textContent === divider_name_content) {
+      mainBody.remove(todo_divider);
+    }
   }
+
 })
 
-// event to add a todo to a dividers
-document.addEventListener("click", (event) =>{
+// event to open a divider
+document.addEventListener("click", (event) => {
   let mainBody = document.querySelector(".main-body");
 
-  if(event.target.classList.contains("side-li")){
-    if(mainBody.querySelector(".todo-divider-tab") === null){
+  if (event.target.classList.contains("side-li")) {
+    let todoTab = document.createElement("div");
+    todoTab.classList.add("todo-divider-tab");
 
-      console.log("there is a child")
-      let todoTab = document.createElement("div");
-      todoTab.classList.add("todo-divider-tab");
+    let dividerName = event.target.firstChild.textContent;
+    let todo_head = document.createElement("div")
+    todo_head.classList.add("todo-head-format")
+    let todoH2 = document.createElement("h2");
+    let i = document.createElement("i")
+    i.setAttribute("class", "add-todo-item")
+    i.classList.add("fa-solid");
+    i.classList.add("fa-plus")
+    todoH2.classList.add("todoH2");
+    todoH2.append(document.createTextNode(dividerName));
+    todo_head.append(todoH2);
+    todo_head.append(i)
+    todoTab.append(todo_head);
 
-      let dividerName = event.target.firstChild.textContent;
-      let todoH2 = document.createElement("h2");
-      todoH2.classList.add("todoH2");
-      todoH2.append(document.createTextNode(dividerName));
-      todoTab.append(todoH2);
-
-      let display = document.querySelector(".display");
-      mainBody.insertBefore(todoTab, display);
-    }
-
+    let display = document.querySelector(".display");
+    mainBody.insertBefore(todoTab, display);
   }
+
+
 })
+
+function createDividers(newDivider) {
+  let newLi = document.createElement("li");
+  newLi.classList.add("side-li")
+  let div = document.createElement("div");
+  div.setAttribute("class", "divider-name")
+  div.append(document.createTextNode(newDivider))
+  let i = document.createElement("i")
+  i.setAttribute("class", "delete-divider")
+  i.classList.add("fa-solid");
+  i.classList.add("fa-x")
+  newLi.append(div);
+  newLi.append(i)
+  document.querySelector(".divider-ul-list").append(newLi);
+
+  let temp = new divider(newDivider);
+  dividers.push(temp);
+
+  localStorage.setItem('dividers', JSON.stringify(dividers));
+}
