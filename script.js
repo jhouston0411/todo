8488249398fc28c6c81563e4aea5ff579f3db54c
jhouston0411 <john.houston0411@gmail.com>
2022-06-22 15:34:12 -0500

    have finished the website might move some javacsript to seperate files

--- script.js
+++ script.js
@@ -6,48 +6,37 @@ function divider(name) {
   this.addTodo = (todoItem) => {
     this.todolist.push(todoItem);
   }
-  this.todo = (title, description, priority) => {
+  this.todo = (title, description, priority, duedate) => {
     return {
       title,
       description,
       priority,
+      duedate
 
     }
   }
 }
 
-// const personal = new divider("personal");
-// const gym = new divider("gym");
-// todo1 = personal.todo("dr", "dentist", "high");
-// todo2 = personal.todo("clun", "fun", "low");
-// gym2 = gym.todo("legs","upper&lower", "medium")
-// gym1 = gym.todo("cardio", "running", "high")
-// gym.addTodo(gym1)
-// personal.addTodo(todo1);
-// gym.addTodo(gym2)
-// personal.addTodo(todo2);
-// dividers.push(personal);
-// dividers.push(gym);
-//
-// for(let i in dividers){
-//
-//   if(dividers[i].dividername === 'personal')
-//     dividers.splice(i, 1);
-// }
-
 window.addEventListener("load", () => {
   let storageDividers = JSON.parse(localStorage.getItem("dividers"));
-  let tempName;
-  for (let divider in storageDividers) {
-    // dividers.push(storageDividers[divider])
-    createDividers(storageDividers[divider].dividername);
+  let storedDivider;
+
+  if (storageDividers.length === 0) {
+       let today = new divider("Today");
+       dividers.push(today);
+       createDividers(today.dividername)
+  } else {
+    for (let stored in storageDividers) {
+      storedDivider = new divider(storageDividers[stored].dividername);
+      storedDivider.todolist = storageDividers[stored].todolist;
+      dividers.push(storedDivider);
+      createDividers(storedDivider.dividername);
+    }
   }
   event.stopPropagation();
 })
 
 //need to put event listener on divider name so it will bubble up to side-li
-// document.addEventListener("click", )
-
 
 // event to add a new divider
 document.addEventListener("click", (event) => {
@@ -84,11 +73,15 @@ document.addEventListener("click", (event) => {
 document.addEventListener("click", (event) => {
 
   if (event.target.classList.contains("submit-green-btn")) {
-    const newDivider = document.querySelector(".divider-input").value;
+    let newDivider = document.querySelector(".divider-input").value;
     if (newDivider != "") {
+      let temp = newDivider;
       createDividers(newDivider);
       document.querySelector("#divider-format").remove()
       document.querySelector(".add-divider-btn").classList.remove("hideAddDivider")
+      temp = new divider(newDivider);
+      dividers.push(temp);
+      localStorage.setItem('dividers', JSON.stringify(dividers));
     } else {
       alert("Please Enter A Todo list Divider Name")
     }
@@ -96,6 +89,7 @@ document.addEventListener("click", (event) => {
     document.querySelector("#divider-format").remove()
     document.querySelector(".add-divider-btn").classList.remove("hideAddDivider")
   }
+
   event.stopPropagation();
 })
 
@@ -131,11 +125,13 @@ document.addEventListener("click", (event) => {
   let todo_divider = document.querySelector(".todo-divider-tab")
 
   if (event.target.classList.contains("side-li")) {
+    let dividerTitle = event.target.firstChild.textContent
     if (todo_divider != null && mainBody.firstElementChild === todo_divider) {
       todo_divider.remove()
-      openDivider(mainBody, event);
+
+      openDivider(mainBody, dividerTitle);
     } else {
-      openDivider(mainBody, event);
+      openDivider(mainBody, dividerTitle);
     }
   }
   event.stopPropagation();
@@ -143,8 +139,10 @@ document.addEventListener("click", (event) => {
 
 //create a todo item for a divider once the add is clicked within the main-body
 document.addEventListener("click", (event) => {
+  let todoDividerContainer = document.querySelector(".todoDividerContainer")
   let display = document.querySelector("#display");
   let messageBoxHeader = document.querySelector("#message-box-header");
+  let todoBoxItems = document.querySelectorAll(".todo-Box")
 
   let sideli = document.querySelector(".divider-ul-list");
   let dividerBtn = document.querySelector(".add-divider-btn");
@@ -154,6 +152,11 @@ document.addEventListener("click", (event) => {
     display.classList.add("active");
     messageBoxHeader.textContent = `${dividerName} Todo Item`;
 
+
+    // while (todoDividerContainer.firstChild) {
+    //   todoDividerContainer.removeChild(todoDividerContainer.firstChild);
+    // }
+
     // disable adding new divider and opening a new divider until user is done with new todo form
     sideli.classList.add("side-li-disable");
     dividerBtn.classList.add("add-divider-btn-disable");
@@ -162,43 +165,84 @@ document.addEventListener("click", (event) => {
 })
 
 //event to save todo item to divider
-document.addEventListener("click", (event) =>{
+document.addEventListener("click", (event) => {
   let display = document.querySelector("#display");
+  let todoDividerContainer = document.querySelector(".todoDividerContainer")
   let sideli = document.querySelector(".divider-ul-list");
   let dividerBtn = document.querySelector(".add-divider-btn");
   let form = new FormData(document.querySelector(".todo-form"));
 
 
-    if(event.target.classList.contains("form-btn-submit")){
-        let dividerName = document.querySelector(".todoH2").textContent;
-        let title = form.get("title");
-        let description = form.get("description");
-        let priority = form.get("priority");
-        let duedate = form.get("duedate");
+  if (event.target.classList.contains("form-btn-submit")) {
+    let dividerName = document.querySelector(".todoH2").textContent;
+    let title = form.get("title");
+    let description = form.get("description");
+    let priority = form.get("priority");
+    let duedate = form.get("duedate");
 
-        for (let i in dividers) {
-          if (dividers[i].dividername === dividerName){
-              let div = dividers[i];
-              console.log(div)
-              temptitle = div.todo(title, description, priority);
-              div.addTodo(temptitle)
-              console.log(div)
-              localStorage.setItem('dividers', JSON.stringify(dividers));
-              console.log(div)
+    if (title === "") {
+      return;
+    }
 
-          }
-        }
+    for (let dividerItem in dividers) {
+      if (dividers[dividerItem].dividername === dividerName) {
+        let tempDivider = dividers[dividerItem];
+        let tempTitle = title;
+        tempTitle = tempDivider.todo(title, description, priority, duedate);
+        tempDivider.addTodo(tempTitle);
+        localStorage.setItem('dividers', JSON.stringify(dividers));
+
+        //clear form and close popUp window
+        document.querySelector(".todo-form").reset();
+        display.classList.remove("active")
+        sideli.classList.remove("side-li-disable");
+        dividerBtn.classList.remove("add-divider-btn-disable");
+        let todoBoxItems = document.querySelectorAll(".todo-Box")
+
+        event.preventDefault();
+        populateTodoItems(tempDivider);
+      }
     }
-    else if(event.target.classList.contains("form-btn-cancel")){
-       display.classList.remove("active")
-       sideli.classList.remove("side-li-disable");
-       dividerBtn.classList.remove("add-divider-btn-disable");
-    }
+  } else if (event.target.classList.contains("form-btn-cancel")) {
+    display.classList.remove("active")
+    sideli.classList.remove("side-li-disable");
+    dividerBtn.classList.remove("add-divider-btn-disable");
+  }
+
 })
 
-//logic to open a divider to main body
+
+// event to handle deleting a todo item
+document.addEventListener("click", (event) => {
+
+  if (event.target.classList.contains("trashTodo")) {
+    let mainDivider = document.querySelector(".todoH2").textContent;
+    let parentRemove = event.target.parentNode.parentNode;
+    let todoListItem = event.target.parentNode.parentNode.firstElementChild.lastElementChild.textContent
+    parentRemove.remove();
+
+    for (let tempDivider in dividers) {
+      if (mainDivider === dividers[tempDivider].dividername) {
+        for (let tempTodo in dividers[tempDivider].todolist) {
+
+          if (todoListItem === dividers[tempDivider].todolist[tempTodo].title) {
+
+            dividers[tempDivider].todolist.splice(tempTodo, 1)
+            localStorage.setItem('dividers', JSON.stringify(dividers));
+          }
+        }
+
+      }
+
+    }
+  }
+})
+
+//logic to add divider to side-ul
 function createDividers(newDivider) {
-  let temp = newDivider;
+  let mainBody = document.querySelector(".main-body");
+  let display = document.querySelector(".display");
+
   let newLi = document.createElement("li");
   newLi.classList.add("side-li")
   let div = document.createElement("div");
@@ -211,19 +255,20 @@ function createDividers(newDivider) {
   newLi.append(div);
   newLi.append(i)
   document.querySelector(".divider-ul-list").append(newLi);
-  // console.log(dividers[0])
-  temp = new divider(newDivider);
-  dividers.push(temp);
-  localStorage.setItem('dividers', JSON.stringify(dividers));
+
+  //if the mainbody is empty open the divider
+  if (mainBody.firstElementChild === display) {
+    openDivider(mainBody, newDivider);
+  }
 
 }
 
 //logic to open a divider if one is open close it first then open the clicked divider
-function openDivider(mainBody, event) {
+function openDivider(mainBody, dividerTitle) {
   let todoTab = document.createElement("div");
   todoTab.classList.add("todo-divider-tab");
 
-  let dividerName = event.target.firstChild.textContent;
+  let dividerName = dividerTitle;
   let todo_head = document.createElement("div")
   todo_head.classList.add("todo-head-format")
   let todoH2 = document.createElement("h2");
@@ -236,7 +281,94 @@ function openDivider(mainBody, event) {
   todo_head.append(todoH2);
   todo_head.append(i)
   todoTab.append(todo_head);
+  let todoDividerContainer = document.createElement("div");
+  todoDividerContainer.setAttribute("class", "todoDividerContainer");
+  todoTab.append(todoDividerContainer);
 
   let display = document.querySelector(".display");
   mainBody.insertBefore(todoTab, display);
+
+  //event to populate the first divider todo list moved to main body
+  for (let temp in dividers) {
+    if (dividers[temp].dividername === dividerTitle) {
+      populateTodoItems(dividers[temp]);
+    }
+  }
+
+}
+
+//function to populate a todo item on main body
+function populateTodoItems(tempDivider) {
+  let todoDividerContainer = document.querySelector(".todoDividerContainer")
+  todoDividerContainer.innerHTML = "";
+  for (todoItem in tempDivider.todolist) {
+
+    let todoBox = document.createElement("div");
+    todoBox.classList.add("todo-Box");
+
+    let titleTodoField = document.createElement("div");
+    titleTodoField.classList.add("divTodoField");
+    let titleh4 = document.createElement("h4");
+    titleh4.classList.add("todoh4");
+    let titleInfo = document.createElement("h4");
+    titleInfo.setAttribute("class", "todoListTitle")
+    titleInfo.classList.add("userInfo")
+
+    let descriptionTodoField = document.createElement("div");
+    descriptionTodoField.classList.add("divTodoField");
+    let descriptionh4 = document.createElement("h4");
+    descriptionh4.classList.add("todoh4");
+    let descriptionInfo = document.createElement("h4");
+    descriptionInfo.classList.add("userInfo");
+
+    let priorityField = document.createElement("div");
+    priorityField.classList.add("divTodoField");
+    let priorityh4 = document.createElement("h4");
+    priorityh4.classList.add("todoh4");
+    let priorityInfo = document.createElement("h4");
+    priorityInfo.classList.add("userInfo")
+
+    let dueDateField = document.createElement("div");
+    dueDateField.classList.add("divTodoField");
+    let duedateh4 = document.createElement("h4");
+    duedateh4.classList.add("todoh4");
+    let duedateInfo = document.createElement("h4");
+    duedateInfo.classList.add("userInfo");
+
+    let todoOptions = document.createElement("div");
+    todoOptions.classList.add("todoOptions");
+    let trashTodo = document.createElement("div");
+    trashTodo.setAttribute("class", "fa-solid fa-trash-can");
+    trashTodo.style.color = "red";
+    trashTodo.classList.add("trashTodo")
+    // let todoComplete = document.createElement("input");
+    // todoComplete.classList.add("todoComplete")
+    // todoComplete.setAttribute("type", "radio");
+    todoOptions.append(trashTodo)
+
+    if (tempDivider.todolist[todoItem].title) {
+      titleh4.append(document.createTextNode("Todo:"));
+      titleInfo.append(document.createTextNode(`${tempDivider.todolist[todoItem].title}`));
+      titleTodoField.append(titleh4, titleInfo);
+    }
+    if (tempDivider.todolist[todoItem].description) {
+      descriptionh4.append(document.createTextNode("Description:"));
+      descriptionInfo.append(document.createTextNode(`${tempDivider.todolist[todoItem].description}`));
+      descriptionTodoField.append(descriptionh4, descriptionInfo);
+    }
+    if (tempDivider.todolist[todoItem].priority) {
+      priorityh4.append(document.createTextNode("Priority:"));
+      priorityInfo.append(document.createTextNode(`${tempDivider.todolist[todoItem].priority}`));
+      priorityField.append(priorityh4, priorityInfo);
+    }
+    if (tempDivider.todolist[todoItem].duedate) {
+      duedateh4.append(document.createTextNode("Duedate:"));
+      duedateInfo.append(document.createTextNode(`${tempDivider.todolist[todoItem].duedate}`));
+      dueDateField.append(duedateh4, duedateInfo);
+    }
+
+    todoBox.append(titleTodoField, descriptionTodoField, priorityField, dueDateField, todoOptions);
+    todoDividerContainer.append(todoBox)
+
+  }
 }
